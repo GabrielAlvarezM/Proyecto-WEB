66043009c4352e050371e215368a62ccf059d11e
Gabriel Álvarez <0261445@up.edu.mx>
2025-09-08 00:59:23 -0600

    Login page y seleccion de menus

---
+++ AppGYM/src/components/DashboardPage.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import './DashboardPage.css';
+
+const DashboardPage = () => {
+  return (
+    <div className="dashboard-container">
+      <Link to="/dietas" className="dashboard-section diet-section">
+        <div className="section-overlay">
+          <h2>Plan de Alimentación</h2>
+        </div>
+      </Link>
+
+      <Link to="/ejercicios" className="dashboard-section exercise-section">
+        <div className="section-overlay">
+          <h2>Planes de Ejercicio</h2>
+        </div>
+      </Link>
+
+      <Link to="/suplementos" className="dashboard-section supplements-section">
+        <div className="section-overlay">
+          <h2>Suplementación y Dónde Conseguirla</h2>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ AppGYM/src/components/LoginPage.tsx
@@ -0,0 +1,49 @@
+
+import React from 'react';
+import './LoginPage.css';
+import { useNavigate } from 'react-router-dom';
+
+const LoginPage = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    //placeholder
+    navigate('/dashboard');
+  };
+
+  return (
+    <div className="login-container">
+      <h1>GYMBUDDY</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="username">Usuario</label>
+          <input type="text" id="username" placeholder="Escribe tu usuario" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Contraseña</label>
+          <input type="password" id="password" placeholder="Escribe tu contraseña" />
+        </div>
+        <div className="button-group">
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
+          <button type="button">Registrarse</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage; 
+
+
+
+
+
+
+
+
+
+
+
