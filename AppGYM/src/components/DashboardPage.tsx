75926438bece2aaaec8142b908a6490b03d56f59
Gabriel Álvarez <0261445@up.edu.mx>
2025-09-13 22:00:54 -0600

    small correction
    si

--- AppGYM/src/components/DashboardPage.tsx
+++ AppGYM/src/components/DashboardPage.tsx
@@ -5,19 +5,19 @@ import './DashboardPage.css';
 const DashboardPage: React.FC = () => {
   return (
     <div className="dashboard-container">
-      <Link to="/dietas" className="dashboard-section diet-section">
+      <Link to="/diets" className="dashboard-section diet-section">
         <div className="section-overlay">
           <h2>Planes de Alimentación</h2>
         </div>
       </Link>
 
-      <Link to="/ejercicios" className="dashboard-section exercise-section">
+      <Link to="/workouts" className="dashboard-section exercise-section">
         <div className="section-overlay">
           <h2>Planes de Ejercicio</h2>
         </div>
       </Link>
 
-      <Link to="/suplementos" className="dashboard-section supplements-section">
+      <Link to="/suplements" className="dashboard-section supplements-section">
         <div className="section-overlay">
           <h2>Suplementación</h2>
         </div>
--- AppGYM/src/components/SuplementPage.tsx
+++ AppGYM/src/components/SuplementPage.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const SupplementPage: React.FC = () => {
   return (
-    <div>
+    <div className = 'Supplements'>
       <h1>Suplementos</h1>
+      
     </div>
   );
 };
